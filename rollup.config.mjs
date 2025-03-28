df0c0963940b5a7de5ef5c4761250482191da7a4
rmcat <rmcat@users.noreply.github.com>
2025-03-29 05:08:42 +1100

    add rollup config to create Firefox extension

---
+++ rollup.config.mjs
@@ -0,0 +1,28 @@
+import resolve from "@rollup/plugin-node-resolve";
+import copy from "rollup-plugin-copy";
+
+export default [
+  {
+    input: "src/scripts/background.js",
+    output: {
+      file: "dist/firefox/scripts/background.bundle.js",
+      format: "iife",
+      sourcemap: true,
+    },
+    plugins: [
+      resolve(),
+      copy({
+        targets: [{ src: "src/manifest.firefox.json", dest: "dist/firefox", rename: "manifest.json" }],
+      }),
+    ],
+  },
+  {
+    input: "src/scripts/content.js",
+    output: {
+      file: "dist/firefox/scripts/content.bundle.js",
+      format: "iife",
+      sourcemap: true,
+    },
+    plugins: [resolve()],
+  },
+];
